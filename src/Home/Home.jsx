fb795ed52d996ee0eaef715df8a9d6c44d7e82d5
mdshakil04 <m.shakil04@yahoo.com>
2023-12-21 12:47:09 +0600

    Some components added

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Banner from './Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is Home</h1>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
