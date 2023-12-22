78d70f941f7ce1388b804c9d30f5091b5dd029b7
mdshakil04 <m.shakil04@yahoo.com>
2023-12-22 12:55:46 +0600

    A little changes done in Front-end

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "./../../Firebase/AuthProvider";
 const Dashboard = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const { user } = useContext(AuthContext);
   return (
     <div className=" ">
       <h2 className=" text-center text-7xl my-4">Add new Task</h2>
-      <div className=" justify-center flex gap-12 mt-8">
-        <button className=" btn btn-outline btn-accent lg:text-2xl">New Task</button>
-        <button className=" btn btn-outline btn-accent lg:text-2xl">See Previous Task</button>
+      <div>
+        <div className="collapse ">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">
+            {user && <p className=" text-2xl font-bold btn btn-outline btn-accent">{user.displayName}</p>}
+          </div>
+          <div className="collapse-content">
+            <div className=" flex gap-4 mt-8">
+              <button className=" btn   lg:text-2xl">
+                New Task
+              </button>
+              <button className=" btn  lg:text-2xl">
+                See Previous Task
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         <div className="hero ">
@@ -41,8 +57,8 @@ const Dashboard = () => {
                   />
                 </div>
                 <label className="label">
-                    <span className="label-text font-bold">Set Priority</span>
-                  </label>
+                  <span className="label-text font-bold">Set Priority</span>
+                </label>
                 <select className="select select-bordered w-full ">
                   <option disabled selected>
                     Priority
@@ -59,7 +75,9 @@ const Dashboard = () => {
                   />
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-accent text-white text-2xl">Add task</button>
+                  <button className="btn btn-accent text-white text-2xl">
+                    Add task
+                  </button>
                 </div>
               </form>
             </div>
